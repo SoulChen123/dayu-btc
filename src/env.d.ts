afb382db8e91bf6d65169abc374359f84b55331b
Soul <soul.chen@coinflex.com>
2023-03-03 09:14:12 +0000

    fix: Delete env.d.ts

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,7 +1,5 @@
 declare global {
   import "@emotion/react";
-  import { ITheme } from "styles/theme";
-  import { DefaultTheme } from "@packages/uikit";
   declare module "@emotion/react" {
     type T = DefaultTheme & ITheme;
     // eslint-disable-next-line @typescript-eslint/no-empty-interface
